fb61d42fb74185a0d831afc7bbc517ad628948d3
JasonFF <925784658@qq.com>
2016-04-27 20:52:55 +0800

    bug

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
     var code = req.query.code;
-    res.render('login', { code: code });
+    res.send('code:'+code);
 });
 
 module.exports = router;
