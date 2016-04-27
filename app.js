3a225bf2393cbf72ef2c0987b802bd4798177398
JasonFF <925784658@qq.com>
2016-04-27 20:32:16 +0800

    修改bug

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.user('/login', login);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
