d688aa2c4056a6d60f2459abfef7c54a4f521ea5
JasonFF <925784658@qq.com>
2016-04-28 11:30:14 +0800

    bug

--- routes/login.js
+++ routes/login.js
@@ -5,8 +5,8 @@ var fetch = require('node-fetch');
 router.get('/', function(req, res, next) {
     var code = req.query.code;
     var appid = 'wx9bf16c912ddec096';
-    var secret = '6841a50bd9b6e99c77d0e052e5e5eb2a';
-    var url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='+appid+'&secret=6841a50bd9b6e99c77d0e052e5e5eb2a&code='+code+'&grant_type=authorization_code';
+    var secret = '6841a50bd9b6e99c77d0e052e5e5eb2a ';
+    var url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='+appid+'&secret=6841a50bd9b6e99c77d0e052e5e5eb2a &code='+code+'&grant_type=authorization_code';
 
     //获取code后，请求以下链接获取access_token：  https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=SECRET&code=CODE&grant_type=authorization_code 
     // fetch(url).then(function(res){
