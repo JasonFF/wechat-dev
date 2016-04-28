835e40020fdba91c1bd4d9bc6e2cfbb1f798401e
JasonFF <925784658@qq.com>
2016-04-28 10:43:54 +0800

    第二步修改

--- routes/login.js
+++ routes/login.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
     var code = req.query.code;
     var appid = 'wx9bf16c912ddec096';
     var secret = '6841a50bd9b6e99c77d0e052e5e5eb2a';
-    var url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='+appid+'&secret='+secret+'&code='+code+'&grant_type=authorization_code';
+    var url = 'https://api.weixin.qq.com/sns/oauth2/access_token';
 
     //获取code后，请求以下链接获取access_token：  https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=SECRET&code=CODE&grant_type=authorization_code 
     // fetch(url).then(function(res){
