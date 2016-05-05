81fed6cf4ad77d61dce4c0591b091377a1c8cb04
JasonFF <925784658@qq.com>
2016-05-05 17:06:29 +0800

    bug

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ var fetch = require('node-fetch');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var appid = 'wx9bf16c912ddec096';
-    var redirect_uri = "http%3A%2F%2Fwww.frombottomto.top/login";
-    var scope = 'snsapi_userinfo';//两种方式
-    var state = 'STATE';//非必须
+    // var appid = 'wx9bf16c912ddec096';
+    // var redirect_uri = "http%3A%2F%2Fwww.frombottomto.top/login";
+    // var scope = 'snsapi_userinfo';//两种方式
+    // var state = 'STATE';//非必须
 
 
-    var url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='+appid+'&redirect_uri='+redirect_uri+'&response_type=code&scope='+scope+'&state='+state+'#wechat_redirect';
+    // var url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='+appid+'&redirect_uri='+redirect_uri+'&response_type=code&scope='+scope+'&state='+state+'#wechat_redirect';
     fetch('dev.91souban.com/user_info').then(function(res){
         console.log(res.json())
     })
