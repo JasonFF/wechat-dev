3da367333b761da778b9211f8498d92453f6281f
JasonFF <925784658@qq.com>
2016-06-01 13:45:23 +0800

    add

--- routes/users.js
+++ routes/users.js
@@ -18,6 +18,7 @@ router.get('/', function(req, res, next) {
 			return res.json()
 		}).then(function(json){
 			jsticket = json.ticket;
+			console.log(req.originalUrl)
 			var string1 = 'jsapi_ticket='+jsticket+'&noncestr=Wm3WZYTPz0wzccnW&timestamp=1414587457&url=http://www.frombottomto.top'+req.originalUrl;
 			res.send(string1)
 		})
